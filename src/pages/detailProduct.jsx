85af1068c1860c606391cc4fb81b1feb18e087ff
rkismyname <arkadiorjndra@gmail.com>
2024-01-10 23:41:36 +0700

    fixed detail product page layout

--- src/pages/detailProduct.jsx
+++ src/pages/detailProduct.jsx
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from "react";
 import MasterNavbar from "../components/masterNavbar";
-import MasterContentImage from "../components/masterContentImage";
 import MasterFooter from "../components/masterFooter";
 import MasterBreadcrumbs from "../components/masterBreadcrumbs";
 import MasterProductImage from "../components/masterProductImage";
-import {
-  Typography,
-  ButtonGroup,
-  Button,
-  Input,
-} from "@material-tailwind/react";
+import { Typography, ButtonGroup, Button, Input } from "@material-tailwind/react";
 
 export default function DetailProduct() {
   const [isNavbarFixed, setIsNavbarFixed] = useState(false);
@@ -37,94 +31,108 @@ export default function DetailProduct() {
         <MasterNavbar />
       </div>
 
-      {/* Breadcrumbs */}
-      {/* <div className="container mx-auto flex justify-start pt-4 xl:px-0 px-4">
-      </div> */}
-
-      <div className="grid grid-cols-2 gap-0 mt-4 px-4 max-w-screen-xl mx-auto">
-        <div className="col-span-1">
-        <MasterBreadcrumbs />
-          <MasterProductImage />
-        </div>
-        <div className="col-span-1">
-          <div className="w-full flex-shrink justify-left">
-            <Typography
-              variant="h3"
-              className="font-bold mb-2 text-black ml-4 mt-4"
-            >
-              Minyak Goreng Curah
-            </Typography>
+      {/* Content */}
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-0 mt-4 px-0 w-full">
+          <div className="sm:order-1">
+            <MasterBreadcrumbs />
+            <MasterProductImage />
           </div>
-          <div className="w-full flex-shrink justify-left">
-            <Typography
-              variant="h2"
-              className="font-bold mb-2 text-black ml-4 mt-4"
-            >
-              Rp 50.000.000 - Rp 100.000.000
-            </Typography>
-          </div>
-          <div className="w-full flex-shrink justify-left border-b">
-            <Typography
-              variant="lead"
-              className="font-bold mb-2 text-black ml-4 mt-4"
-            >
-              Availability: In Stock
-            </Typography>
-          </div>
-          <div className="w-full flex gap-8 justify-left">
-            <div className="flex items-center justify-center">
+          <div className="sm:order-2">
+            <div className="w-full flex-shrink justify-left">
               <Typography
-                variant="h5"
+                variant="h3"
                 className="font-bold mb-2 text-black ml-4 mt-4"
               >
-                Quantity:
+                Minyak Goreng Curah
               </Typography>
             </div>
-            <div className="flex items-center w-[150px]">
-              <ButtonGroup size="md" className="w-[150px]">
+            <div className="w-full flex-shrink justify-left">
+              <Typography
+                variant="h2"
+                className="font-bold mb-2 text-black ml-4 mt-4"
+              >
+                Rp 50.000.000 - Rp 100.000.000
+              </Typography>
+            </div>
+            <div className="w-full flex-shrink justify-left border-b">
+              <Typography
+                variant="lead"
+                className="font-bold mb-2 text-black ml-4 mt-4"
+              >
+                Availability: In Stock
+              </Typography>
+            </div>
+            <div className="w-full flex items-center justify-left ml-4 mt-4">
+              <div className="">
+                <Typography variant="h5" className="font-bold mb-2 text-black">
+                  Quantity:
+                </Typography>
+              </div>
+              <ButtonGroup size="md" className="ml-4 justify-center">
                 <Button className="hover:bg-blue-400 bg-wpiblue-50">-</Button>
                 <input
                   type="qty"
                   className="w-[45px] max-w-[45px] border text-center"
                   placeholder="Qty."
-                ></input>
-                <Button className="hover:bg-blue-400 bg-wpiblue-50 ">+</Button>
+                />
+                <Button className="hover:bg-blue-400 bg-wpiblue-50">+</Button>
               </ButtonGroup>
             </div>
+            <div className="w-full flex items-center justify-center mt-4">
+              <Button className="hover:bg-green-400 bg-wpigreen-50 w-full">
+                Hubungi Kami
+              </Button>
+            </div>
           </div>
-          <div className="flex items-center justify-left pt-8 px-3.5">
-            <Button className="hover:bg-green-400 bg-wpigreen-50">
-              Hubungi Kami
-            </Button>
-          </div>
-        </div>
-        <div className="col-span-2">
-          <div className="w-full flex-shrink justify-left border-b">
-            <Typography
-              variant="lead"
-              className="font-bold mb-2 text-black ml-4 mt-4"
-            >
-              Berat
-            </Typography>
-          </div>
-          <div className="w-full flex-shrink justify-left border-b">
-            <Typography
-              variant="lead"
-              className="font-bold mb-2 text-black ml-4 mt-4"
-            >
-              Satuan
-            </Typography>
-          </div>
-          <div className="w-full flex-shrink justify-left border-b pb-16">
-            <Typography
-              variant="lead"
-              className="font-bold mb-2 text-black ml-4 mt-4"
-            >
-              Diambil Dari
-            </Typography>
+          <div className="sm:order-3 flex pb-20 col-span- w-full">
+            <div className="w-[210px] items-left justify-left">
+              <div className="w-full justify-left items-left border-b ml-4 lg:ml-0">
+                <Typography
+                  variant="lead"
+                  className="font-bold mb-2 text-black mt-4"
+                >
+                  Berat
+                </Typography>
+              </div>
+              <div className="w-full justify-left items-left border-b ml-4 lg:ml-0">
+                <Typography
+                  variant="lead"
+                  className="text-black mt-4 font-bold"
+                >
+                  Satuan
+                </Typography>
+              </div>
+              <div className="w-full justify-left items-left border-b ml-4 lg:ml-0">
+                <Typography
+                  variant="lead"
+                  className="text-black mt-4 font-bold"
+                >
+                  Diambil Dari
+                </Typography>
+              </div>
+            </div>
+            <div className="w-full items-left justify-left">
+              <div className="w-full justify-left items-left border-b pt-[8px]">
+                <Typography variant="lead" className="text-black mt-4">
+                  5000L
+                </Typography>
+              </div>
+              <div className="w-full justify-left items-left border-b">
+                <Typography variant="lead" className="text-black mt-4">
+                  Liter
+                </Typography>
+              </div>
+              <div className="w-full justify-left items-left border-b">
+                <Typography variant="lead" className="text-black mt-4">
+                  Kalimantan Barat
+                </Typography>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
       {/* Form Email */}
       <div className="bg-blue-50 lg:px-28 px-0 h-[50px]">
         <div className=" bg-white grid grid-cols-1 lg:grid-cols-12 border rounded-lg shadow-lg py-8 px-4 mx-8 lg:mx-0 -translate-y-10">
@@ -151,6 +159,7 @@ export default function DetailProduct() {
         </div>
       </div>
 
+      {/* Footer */}
       <div className="bg-blue-50">
         <div className="container mx-auto pt-20 lg:pt-8">
           <MasterFooter />
