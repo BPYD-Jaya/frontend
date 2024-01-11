3cd05ea5cd524844df671824284f5b8507b8d697
farhandwicahyo <farhandwicahyoo@gmail.com>
2024-01-11 22:20:46 +0700

    responsive cms news&product

--- src/pages/AdminProduct.jsx
+++ src/pages/AdminProduct.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MasterSidebar from "../components/masterSidebar";
 import { Button, Card, Typography } from "@material-tailwind/react";
 import MasterFooterAdmin from "../components/masterFooterAdmin";
@@ -9,18 +9,46 @@ import { FaMagnifyingGlass } from "react-icons/fa6";
 import MasterCatalogAdmin from "../components/masterCatalogAdmin";
 
 export default function AdminProduct() {
+  const [openSidebar, setOpenSidebar] = useState(window.innerWidth >= 640);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setOpenSidebar(window.innerWidth >= 640);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div className="bg-gray-100 h-full flex flex-col min-h-screen">
       {/* Sidebar */}
-      <div className="bg-white z-50 fixed top-0 h-full">
+      <div
+        className={`bg-white z-50 fixed top-0 h-full md:block transition-transform duration-200 ease-in-out ${
+          openSidebar ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
         <MasterSidebar />
       </div>
 
+      {openSidebar && (
+        <div
+          className="fixed inset-0 bg-black z-40 transition-opacity duration-200 ease-in-out opacity-50 md:hidden "
+          onClick={() => setOpenSidebar(false)}
+        ></div>
+      )}
+
       {/* Navbar */}
-      <MasterNavbarAdmin />
+      <MasterNavbarAdmin
+        openSidebar={openSidebar}
+        setOpenSidebar={setOpenSidebar}
+      />
 
       {/* Content Product */}
-      <div className="flex-grow h-full ml-80 pt-10 mr-0">
+      <div className=" h-full ml-6 md:ml-80 pt-10 mr-0">
         <div className="grid grid-cols-4 gap-8 bg-white mr-6 mb-6 pt-4 pl-6 rounded-lg shadow-md ">
           <Typography className="col-span-2 flex items-center">
             Product
@@ -48,7 +76,7 @@ export default function AdminProduct() {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-4 gap-8 bg-white mr-6 mb-6 pt-6 pb-6 pr-2 pl-6 rounded-lg shadow-md">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-8 bg-white w-auto mr-6 mb-6 pt-6 pb-6 pr-6 pl-6 justify-center items-center rounded-lg shadow-md">
           <MasterCatalogAdmin />
           <MasterCatalogAdmin />
           <MasterCatalogAdmin />
