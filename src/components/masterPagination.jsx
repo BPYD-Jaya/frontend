b2d112e99a7631d5bc7eb778314e81ecf528f734
Putra Fajar <116268708+putrafjr@users.noreply.github.com>
2024-01-14 13:32:55 +0700

    Update Comprof

--- src/components/masterPagination.jsx
+++ src/components/masterPagination.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Button, IconButton } from "@material-tailwind/react";
-import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
+import React from 'react';
+import { Button, IconButton } from '@material-tailwind/react';
+import { ArrowRightIcon, ArrowLeftIcon } from '@heroicons/react/24/outline';
 
 export function DefaultPagination() {
   const [active, setActive] = React.useState(1);
 
   const getItemProps = (index) => ({
-    variant: active === index ? "filled" : "text",
-    color: "green",
+    variant: active === index ? 'filled' : 'text',
+    color: 'green',
     onClick: () => setActive(index),
   });
 
@@ -24,25 +24,23 @@ export function DefaultPagination() {
   };
 
   return (
-    <div className="flex items-center gap-">
+    <div className="flex items-center w-full">
       <Button
         variant="text"
-        className="flex items-center text-s"
+        className="flex items-center text-s gap-2"
         onClick={prev}
         disabled={active === 1}
       >
         <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" /> Previous
       </Button>
-      <div className="flex items-center">
+      <div className="flex items-center ">
         <IconButton {...getItemProps(1)}>1</IconButton>
         <IconButton {...getItemProps(2)}>2</IconButton>
         <IconButton {...getItemProps(3)}>3</IconButton>
-        <IconButton {...getItemProps(4)}>4</IconButton>
-        <IconButton {...getItemProps(5)}>5</IconButton>
       </div>
       <Button
         variant="text"
-        className="flex items-center text-s"
+        className="flex items-center text-s gap-2"
         onClick={next}
         disabled={active === 5}
       >
--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -132,35 +132,32 @@ export default function NewsPage() {
 
         {/* Form Email */}
         <div className="bg-blue-50 lg:px-28 px-0 h-[50px]">
-          <div className=" bg-white grid grid-cols-1 lg:grid-cols-12 border rounded-lg shadow-lg py-8 px-4 mx-8 lg:mx-0 -translate-y-10">
-            <div className=" col-span-6 text-center flex items-center justify-center lg:justify-start">
-              <Typography
-                variant="h4"
-                style={{
-                  fontFamily: "'M PLUS Rounded 1c', sans-serif",
-                  fontWeight: 700,
+        <div className=" bg-white grid grid-cols-1 lg:grid-cols-12 border rounded-lg shadow-lg py-8 px-4 mx-8 lg:mx-0 -translate-y-10">
+          <div className=" col-span-6 text-center flex items-center justify-center lg:justify-start">
+            <Typography variant="h4"style={{
+                fontFamily: "'M PLUS Rounded 1c', sans-serif",
+                fontWeight: 700,
+              }}>
+              Dapatkan info menarik dari kami!
+            </Typography>
+          </div>
+          <div className=" col-span-6">
+            <div className="flex gap-2">
+              <Input
+                size="lg"
+                placeholder="Email address"
+                className=" !border-t-blue-gray-200 focus:!border-t-gray-900 "
+                labelProps={{
+                  className: "before:content-none after:content-none w-full",
                 }}
-              >
-                Dapatkan info menarik dari kami!
-              </Typography>
-            </div>
-            <div className=" col-span-6">
-              <div className="flex gap-2">
-                <Input
-                  size="lg"
-                  placeholder="Email address"
-                  className=" !border-t-blue-gray-200 focus:!border-t-gray-900 "
-                  labelProps={{
-                    className: "before:content-none after:content-none w-full",
-                  }}
-                />
-                <Button className="hover:bg-green-400 bg-wpigreen-50">
-                  Submit
-                </Button>
-              </div>
+              />
+              <Button className="hover:bg-green-400 bg-wpigreen-50">
+                Submit
+              </Button>
             </div>
           </div>
         </div>
+      </div>
 
         {/* Footer */}
         <div className="bg-blue-50">
