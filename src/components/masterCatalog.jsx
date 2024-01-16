7db8d46b573789c974f16869ee1a67a434c4519e
farhandwicahyo <farhandwicahyoo@gmail.com>
2024-01-16 18:23:55 +0700

    deatil product update

--- src/components/masterCatalog.jsx
+++ src/components/masterCatalog.jsx
@@ -4,48 +4,54 @@ import {
   CardHeader,
   CardBody,
   Typography,
-  Button,
 } from "@material-tailwind/react";
 import MasterButtonWa from "./masterButtonWa";
 
-export default function MasterCatalog() {
+export default function MasterCatalog({
+  imageUrl,
+  productName,
+  priceRange,
+  minOrder,
+}) {
   return (
     <Card className="lg:max-w-[250px] w-full overflow-hidden">
-      <CardHeader 
+      <CardHeader
         floated={false}
         shadow={false}
         color="transparent"
-        className=" object-cover transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out"
+        className="object-cover transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out"
       >
         <a href="/detailproduk">
-          <img className="rounded-md"
-            src="https://mitrawarungpangan.bgrlogistics.id/upload/thumbs/512/314b8961ed526933bec7c95a57549f6a.jpg"
-            alt="ui/ux review check"
-          />
+          <img className="rounded-md" src={imageUrl} alt={productName} />
         </a>
       </CardHeader>
       <CardBody className="p-6">
-        <Typography variant="h4" color="blue-gray" className="mb-2 text-xl whitespace-nowrap font-semibold" style={{ fontSize: '19px' }}>
-          Minyak Goreng Curah
+        <Typography
+          variant="h4"
+          color="blue-gray"
+          className="text-xl whitespace-nowrap font-semibold"
+          style={{ fontSize: "19px" }}
+        >
+          {productName}
         </Typography>
         <Typography
           variant="lead"
           color="black"
-          className="mb-4 font-normal whitespace-nowrap"
-          style={{ fontSize: '0.950rem' }}
+          className="font-bold whitespace-nowrap"
+          style={{ fontSize: "0.950rem" }}
         >
-          Rp 100 Juta - 500 Juta
+          {priceRange}
         </Typography>
-        <div className="flex items-center justify-between mb-2">
-          <Typography variant="body2" color="gray" className="font-normal text-wpigreen-50 italic whitespace-nowrap text-xs">
-            Gratis Pengiriman
-          </Typography>
-          <Typography variant="caption" color="gray" className="font-normal whitespace-nowrap text-xs">
-            Terjual 1000
-          </Typography>
-        </div>
+        <Typography
+          variant="lead"
+          color="black"
+          className="font-normal whitespace-nowrap"
+          style={{ fontSize: "0.950rem" }}
+        >
+          Min. order: {minOrder}
+        </Typography>
         <div className="flex items-center justify-center mb-2">
-            <MasterButtonWa />
+          <MasterButtonWa />
         </div>
       </CardBody>
     </Card>
