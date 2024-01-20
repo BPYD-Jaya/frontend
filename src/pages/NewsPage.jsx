991734e376d585d58674aaaa70163df0a6ab6ea7
rkismyname <arkadiorjndra@gmail.com>
2024-01-20 23:39:30 +0700

    pagination news

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import MasterNavbar from '../components/masterNavbar';
-import { Button, Input, Typography } from '@material-tailwind/react';
-import { FaRegNewspaper } from 'react-icons/fa6';
-import MasterCard from '../components/masterCard';
-import MasterFooter from '../components/masterFooter';
-import MasterNews from '../components/masterNews';
+import React, { useState, useEffect } from "react";
+import MasterNavbar from "../components/masterNavbar";
+import { Button, Input, Typography } from "@material-tailwind/react";
+import { FaRegNewspaper } from "react-icons/fa6";
+import MasterCard from "../components/masterCard";
+import MasterFooter from "../components/masterFooter";
+import MasterNews from "../components/masterNews";
+import MasterPagination from "../components/masterPagination";
 
 export default function NewsPage() {
   const [isNavbarFixed, setIsNavbarFixed] = useState(false);
@@ -15,10 +16,10 @@ export default function NewsPage() {
       setIsNavbarFixed(scrollTop > 0);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -27,7 +28,7 @@ export default function NewsPage() {
       {/* Navbar */}
       <div
         className={`bg-wpiblue-50 ${
-          isNavbarFixed ? 'fixed top-0 w-full z-10' : ''
+          isNavbarFixed ? "fixed top-0 w-full z-10" : ""
         }`}
       >
         <MasterNavbar />
@@ -37,7 +38,7 @@ export default function NewsPage() {
       <div className="">
         <div
           className="bg-gradient-to-t from-wpigreen-50 to-wpiblue-50 flex flex-col justify-center items-center px-4 xl:px-36 xl:h-[400px] md:h-[500px] lg:h-[450px] h-[500px] pb-16"
-          style={{ borderRadius: '0 0 50px 50px' }}
+          style={{ borderRadius: "0 0 50px 50px" }}
         >
           <div className="flex flex-col justify-center items-center text-center text-white">
             <FaRegNewspaper size={100} className="mb-2" />
@@ -117,20 +118,23 @@ export default function NewsPage() {
               <MasterNews />
             </a>
           </div>
+          <div className="flex lg:justify-center lg:pr-[435px]  items-center mt-6">
+            <MasterPagination />
+          </div>
         </div>
 
         {/* Content Image */}
         <div className="container mx-auto flex justify-center pb-16 px-8 my-6">
-        <div className="overflow-hidden transform transition-transform duration-300 hover:scale-105">
-          <a href="/produk">
-            <img
-              src="https://warungpangan.com/upload/settings/mb_banner_bottom.png"
-              alt=""
-              className="w-full"
-            />
-          </a>
+          <div className="overflow-hidden transform transition-transform duration-300 hover:scale-105">
+            <a href="/produk">
+              <img
+                src="https://warungpangan.com/upload/settings/mb_banner_bottom.png"
+                alt=""
+                className="w-full"
+              />
+            </a>
+          </div>
         </div>
-      </div>
 
         {/* Form Email */}
         <div className="bg-blue-50 lg:px-28 px-0 h-[50px]">
