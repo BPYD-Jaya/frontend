dca417fff9b14c345234c8b73d9f7ea5abe16830
Putra Fajar <116268708+putrafjr@users.noreply.github.com>
2024-01-27 12:56:17 +0700

    fix master city

--- src/pages/MasterCity.jsx
+++ src/pages/MasterCity.jsx
@@ -22,14 +22,12 @@ const MasterCity = () => {
   });
   console.log('paginationData',paginationData)
   console.log('province', provinsi)
-  console.log('paginationData',paginationData)
-  console.log('province', provinsi)
 
   const onProvinsi = async () => {
     try {
       const response = await axios.get(`https://backend.ptwpi.co.id/api/provinces`);
       setProvinsi(response.data);  // Make sure to set 'provinsi' with the data property
-      setProvinsi(response.data);  // Make sure to set 'provinsi' with the data property
+    
     } catch (error) {
       console.log(error);
     }
@@ -81,7 +79,6 @@ const MasterCity = () => {
 
   useEffect(() => {
   }, [provinsi]);
-  }, [provinsi]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -100,14 +97,6 @@ const MasterCity = () => {
     // Initial data fetch on component mount
     fetchData(currentPage);
   }, [currentPage]);
-
-  const getProvinceName = (id) => {
-    const matchingProvince = provinsi.find(province => province.id === id);
-    // Access the 'province' property to get the name, if a matching province was found
-    const provinceName = matchingProvince ? matchingProvince.province : 'Province Not Found';
-
-    return provinceName
-  }
 
   const getProvinceName = (id) => {
     const matchingProvince = provinsi.find(province => province.id === id);
@@ -177,17 +166,14 @@ const MasterCity = () => {
               </thead>
               <tbody>
                 {paginationData.data.map((data, idx) => (
-                {paginationData.data.map((data, idx) => (
                   <tr key={data.nomor} className="even:bg-blue-gray-50/50">
                     <td className="p-4">
                       <Typography variant="small" color="blue-gray" className="font-normal">
-                        {idx + 1 +( paginationData.current_page * 10 - 10)}
                         {idx + 1 +( paginationData.current_page * 10 - 10)}
                       </Typography>
                     </td>
                     <td className="p-4">
                       <Typography variant="small" color="blue-gray" className="font-normal">
-                        {getProvinceName(data.province_id)}
                         {getProvinceName(data.province_id)}
                       </Typography>
                     </td>
@@ -215,7 +201,6 @@ const MasterCity = () => {
                               >
                                 <path
                                   strokeLinecap="round"
-                                  strokeLinejoin="round"
                                   d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
                                 />
                               </svg>
@@ -238,7 +223,6 @@ const MasterCity = () => {
                             >
                               <path
                                 strokeLinecap="round"
-                                strokeLinejoin="round"
                                 d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
                               />
                             </svg>
